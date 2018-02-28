1cfd6f67772fc8dcfcac581dc03f8221bb6079a7
KittenDeveloper <fuller.justice@gmail.com>
2018-02-28 10:55:55 -0600

    Fix and improve some things
    -Python 2-3 compatible
    -Changed some while loops to for loops
    -Changed englishLetters to string.ascii_letters
    -Closed open files when done using
    -Changed FindFile to a loop (Previously recursion)
    -Changed location argument to fileStr argument
    -Used string slicing to print first 4 chars
    -Replaced counters with range()
    -Added casts to str in some print calls
    -Removed some boolean based loops
    -Other various changes

--- DeCode.py
+++ DeCode.py
@@ -1,9 +1,9 @@
+import sys
+if sys.version_info[0]>2:raw_input=input
 import os
-
+import string
 def Start():
-	print ""
-	print "D:\Code initially by Aidan Welch"
-	print ""
+	print("\nD:\Code initially by Aidan Welch\n")
 
 	print("This is merely a tool to assist in deciphering, not an end all be all solution.  Meaning I can't guarantee this will help you.")
 	raw_input("Press Enter(Return) to continue...")
@@ -12,51 +12,45 @@ def Start():
 
 def FindFile():
 	location = raw_input("Input your file's location: ")
-	if len(location) < 1:
-		print ("You didn't type anything")
-		FindFile()
-	else:
-		 #Opens the file and finds it
-		 file = open(location, "r")
-		 print "Following will be the first 4 characters of " + os.path.basename(location) + ":"
-		 print file.read(4)
-		 unanswered = True
-		 while unanswered == True:
-		 	answer = raw_input("Type Y if correct or N if false: ")
-		 	if answer.upper() == "Y":
-		 		TallyChars(location)
-		 		unanswered = False
-		 	elif answer.upper() == "N":
-		 		FindFile()
-		 		unanswered = False
-		 	else:
-		 		unanswered = True
-
-
-def TallyChars(location):
-	#must reopen file because everytime file is read it saves cursor position
+	while not len(location):
+		location = raw_input("You didn't type anything, try again: ")
+	#Opens the file and finds it
 	file = open(location, "r")
-	fileStr = file.read()
+	print("Following will be the first 4 characters of " + os.path.basename(location) + ":")
+	fileStr=file.read()
+	print(fileStr[0:4])
+	file.close()
+	while True:
+		answer = raw_input("Type Y if correct or N if false: ")
+		if answer.upper() == "Y":
+			TallyChars(location)
+			break
+		elif answer.upper() == "N":
+			FindFile()
+			break
+		else:
+			continue
+
+
+def TallyChars(fileStr):
+	#must reopen file because everytime file is read it saves cursor position
 	charList = []
 	charCount = []
-	i = 0
 	#Declaring Arrays and stuff ^
- 	while i < len(fileStr):
-		charCur = fileStr[i]
+	for charCur in fileStr:
 		if((charCur in charList) == False):
 		 	charList.append(charCur)
 		 	charCount.append(1) 		
 		else:
 		 	currentPoint = charList.index(charCur)
 			charCount[currentPoint] = charCount[currentPoint] + 1
-		
- 		i = i + 1
- 	#Character Tally^^^
- 	print "Here are the characters counted:"
- 	print charList
- 	print "Here are the amount of times each appeared:"
- 	print charCount
- 	AdditionalCharInfo(charList, charCount, location)
+
+	#Character Tally^^^
+ 	print("Here are the characters counted:")
+	print(str(charList))
+	print("Here are the amount of times each appeared:"
+	print(str(charCount))
+	AdditionalCharInfo(charList, charCount, location)
 	unanswered = False
 
 
@@ -65,22 +59,19 @@ def AdditionalCharInfo(unsortedList, unsortedCount, location):
 	charList = [] #this is different from the old charCount and charList for simplification; this is sorted list
 	charPer = [] #this is sorted percents
 	total = sum(unsortedCount)
-	i = 0
-	while i < len(unsortedCount):
-		percentCount.append(float(int((float(unsortedCount[i])/float(total))*100000))/1000)
-		i = i + 1
+	for i in unsortedCount:
+		percentCount.append(float(int((float(i)/float(total))*100000))/1000)
 	#converting to percentages and rounding/simplyifying number^
-	print "Here is the count converted to percentages:"
-	print percentCount
-	print "And this is the accuracy of the those percentages:"
-	print str(sum(percentCount)) + "%"
+	print("Here is the count converted to percentages:")
+	print(percentCount)
+	print("And this is the accuracy of the those percentages:")
+	print(str(sum(percentCount)) + "%")
 	charPer = sorted(percentCount)
 	charList = SortChars(unsortedList, percentCount)
 	print"Here are the characters sorted from least to greatest:"
 	i = 0
-	while i < len(charList):
-		print "'"+ str(charList[i]) + "'                        " + str(charPer[i])
-		i = i + 1
+	for i in range(len(charList)):
+		print "'"+ str(charList[i]) + "'"+(' '*24)+ str(charPer[i])
 	#print charList
 	#print "And here is the percentages of each:"
 	#rint charPer
@@ -115,20 +106,20 @@ def SortChars(unsortedList, percentCount):
 	return charList
 """
 def CompareChars(charList, charPer):
-	englishLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	i = 0
+#	englishLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+#Switch to string.ascii_uppercase
 	charIncluded = []
 	perIncluded = []
-	while i < len(charList):
-		if charList[i].upper() in englishLetters and charList[i].upper() not in englishLetters:
-			charIncluded.append(charList[i].upper())
+	for i in range(len(charList)):
+		if charList[i] in string.ascii_letters and charList[i] not in string.ascii_letters:
+			charIncluded.append(charList[i])
 			perIncluded.append(charPer[i])
 		else:
 			print charList[i] + " is not in the English alphabet"
-		i += 1
 	print charIncluded
 	print perIncluded
 	alphabet = open('/alphabet.txt', "r")
+	alphabet.close()
 #what I have so far in a soon to be tool to compare to the english alphabet and eventually de-cipher
 """
 	
