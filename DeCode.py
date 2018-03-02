57360649fd1f20107d4d42cdde02db0bf6b49603
KittenDeveloper <fuller.justice@gmail.com>
2018-03-02 16:48:34 -0600

    Complete style change (Read description)
    Style Changes:
    -Functions return values
    -Functions no longer output (print) return values
    -Start function moved to bottom
    -Start function outputs all info together
    Other Changes:
    -Fix CompareChars for Python 3
    -Tested on Python 3.6.2 64-bit
    -Changed print format

--- DeCode.py
+++ DeCode.py
@@ -1,16 +1,11 @@
 import sys
-if sys.version_info[0]>2:raw_input=input
+if sys.version_info[0]>2:raw_input=input #Checks for Python3 adds raw_input
 import os
 import string
 yesValues=["yes", "y", "yeah", "yep", "yea", "ok", "okay", "true"]
-def Start():
-	print("\nD:\\Code initially by Aidan Welch\n")
-
-	print("This is merely a tool to assist in deciphering, not an end all be all solution.  Meaning I can't guarantee this will help you.")
-	raw_input("Press Enter(Return) to continue...")
-	FindFile()
 
 def FindFile():
+	"Gets file from user and returns a string of the file" #Docstring
 	while True:
 		location = raw_input("Input your file's location: ")
 		if not len(location): location= raw_input("You didn't type anything, try again: ")
@@ -29,49 +24,33 @@ def FindFile():
 		while True:
 			answer = raw_input("Type Y if correct or N if false: ")
 			if not answer: print("Nothing typed, try again."); continue
-			if answer.lower() in yesValues:
-				TallyChars(fileStr)
-				return
+			if answer.lower() in yesValues: return fileStr
 			else: break #Incorrect try again
 
-
 def TallyChars(fileStr):
+	"Counts each charecter in a string and returns a dictionary of the char and the count" #Docstring
 	charCount = {}
 	#Declaring Arrays and stuff ^
 	for charCur in fileStr:
 		if not (charCur in charCount):
-			charCount[charCur]=1
+			charCount[charCur]={"count": 1}
 		else:
-			charCount[charCur] += 1
+			charCount[charCur]["count"]+= 1
 	#Character Tally^^^
-	print("Here are the characters counted and the amount of times each appeared:")
-	for charCur in charCount:
-		print("'"+charCur+"': "+str(charCount[charCur]))
-	AdditionalCharInfo(list(charCount.keys()), charCount.values())
+	return charCount
 
-def AdditionalCharInfo(unsortedList, unsortedCount):
-	percentCount = []
-	charList = [] #this is different from the old charCount and charList for simplification; this is sorted list
-	charPer = [] #this is sorted percents
-	total = sum(unsortedCount)
-	for i in unsortedCount:
-		percentCount.append(float(int((float(i)/float(total))*100000))/1000)
+def AdditionalCharInfo(charInfo):
+	"Gets percentages for chars" #Docstring
+	total = sum([charInfo[char]["count"] for char in charInfo])
+	for char in charInfo:
+		charInfo[char]["percent"]=float(int((float(charInfo[char]["count"])/float(total))*100000))/1000
 	#converting to percentages and rounding/simplyifying number^
-	print("Here is the count converted to percentages:")
-	print(percentCount)
-	print("And this is the accuracy of the those percentages:")
-	print(str(sum(percentCount)) + "%")
-	charPer = sorted(percentCount)
-	charList = SortChars(unsortedList, percentCount)
-	print("Here are the characters sorted from least to greatest:")
-	i = 0
-	for i in range(len(charList)):
-		print("'"+ str(charList[i]) + "'"+(' '*24)+ str(charPer[i]))
 	#print charList
 	#print "And here is the percentages of each:"
 	#rint charPer
 	#above is what i had before I improved the format
 	#CompareChars(charList, charPer)
+	return charInfo
 
 def SortChars(unsortedList, percentCount):
 	i = 0
@@ -99,6 +78,17 @@ def SortChars(unsortedList, percentCount):
 			charList.insert(x, unsortedList[i])
 		i += 1
 	return charList
+def SortCharInfo(charInfo):
+	"Sorts char info from least to greatest" #Docstring
+	chars=list(charInfo.keys())
+	charCounts=[charInfo[char]["count"] for char in list(charInfo.keys())]
+	charPercents=[charInfo[char]["percent"] for char in list(charInfo.keys())]
+	propertyArr=[chars, charCounts, charPercents]
+	sortedArr=sorted([(propertyArr[0][i], propertyArr[1][i], propertyArr[2][i]) for i in range(len(chars))], key= lambda char: char[1])
+	sortedDict={}
+	for i in range(len(chars)):
+		sortedDict[sortedArr[i][0]]={"count": sortedArr[i][1], "percent": sortedArr[i][2]}
+	return sortedDict
 """
 def CompareChars(charList, charPer):
 #	englishLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -110,9 +100,9 @@ def CompareChars(charList, charPer):
 			charIncluded.append(charList[i])
 			perIncluded.append(charPer[i])
 		else:
-			print charList[i] + " is not in the English alphabet"
-	print charIncluded
-	print perIncluded
+			print(charList[i] + " is not in the English alphabet")
+	print(charIncluded)
+	print(perIncluded)
 	try:
 		alphabet = open('/alphabet.txt', "r")
 		alphabet.close()
@@ -121,6 +111,20 @@ def CompareChars(charList, charPer):
 
 #what I have so far in a soon to be tool to compare to the english alphabet and eventually de-cipher
 """
-	
+def Start():
+	"Entry point" #Docstring
+	print("\nD:\\Code initially by Aidan Welch\n")
+	print("This is merely a tool to assist in deciphering, not an end all be all solution.  Meaning I can't guarantee this will help you.")
+	raw_input("Press Enter(Return) to continue...")
+	fileStr=FindFile()
+	charInfo=TallyChars(fileStr)
+	charInfo=AdditionalCharInfo(charInfo)
+	charInfo=SortCharInfo(charInfo)
+	charsCounted=len(list(charInfo.keys()))
+	percentAccuracy=sum([charInfo[char]["percent"] for char in charInfo])
+	print("Information about the "+str(charsCounted)+" chars counted from least to greatest")
+	print("Note that percentage is ONLY accurate up to "+str(percentAccuracy)+"%")
+	for char in charInfo:
+		print("Char '%s': Count=%s, Percentage=%s" % (char, charInfo[char]["count"], charInfo[char]["percent"]))
 
 Start()
