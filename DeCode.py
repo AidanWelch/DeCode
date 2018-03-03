282fec8bd7534ad8490412c334766e526fedfa26
KittenDeveloper <fuller.justice@gmail.com>
2018-03-02 19:54:47 -0600

    Use decimal instead of float/ CompareChars
    -float to decimal.Decimal for precision/better arithmatic
    -VERY primitive version of CompareChars that returns a alphabet map for each charecter

--- DeCode.py
+++ DeCode.py
@@ -1,6 +1,9 @@
+import collections
+import decimal
 import sys
 if sys.version_info[0]>2:raw_input=input #Checks for Python3 adds raw_input
 import os
+import re
 import string
 yesValues=["yes", "y", "yeah", "yep", "yea", "ok", "okay", "true"]
 
@@ -29,7 +32,7 @@ def FindFile():
 
 def TallyChars(fileStr):
 	"Counts each charecter in a string and returns a dictionary of the char and the count" #Docstring
-	charCount = {}
+	charCount = collections.OrderedDict()
 	#Declaring Arrays and stuff ^
 	for charCur in fileStr:
 		if not (charCur in charCount):
@@ -43,7 +46,7 @@ def AdditionalCharInfo(charInfo):
 	"Gets percentages for chars" #Docstring
 	total = sum([charInfo[char]["count"] for char in charInfo])
 	for char in charInfo:
-		charInfo[char]["percent"]=float(int((float(charInfo[char]["count"])/float(total))*100000))/1000
+		charInfo[char]["percent"]=decimal.Decimal(int((decimal.Decimal(charInfo[char]["count"])/decimal.Decimal(total))*100000))/1000
 	#converting to percentages and rounding/simplyifying number^
 	#print charList
 	#print "And here is the percentages of each:"
@@ -85,12 +88,22 @@ def SortCharInfo(charInfo):
 	charPercents=[charInfo[char]["percent"] for char in list(charInfo.keys())]
 	propertyArr=[chars, charCounts, charPercents]
 	sortedArr=sorted([(propertyArr[0][i], propertyArr[1][i], propertyArr[2][i]) for i in range(len(chars))], key= lambda char: char[1])
-	sortedDict={}
+	sortedDict=collections.OrderedDict()
 	for i in range(len(chars)):
 		sortedDict[sortedArr[i][0]]={"count": sortedArr[i][1], "percent": sortedArr[i][2]}
 	return sortedDict
-"""
-def CompareChars(charList, charPer):
+def SortCharPercents(charInfo):
+	"Sorts char percents from least to greatest" #Docstring
+	chars=list(charInfo.keys())
+	charPercents=[charInfo[char]["percent"] for char in list(charInfo.keys())]
+	propertyArr=[chars, charPercents]
+	sortedArr=sorted([(propertyArr[0][i], propertyArr[1][i]) for i in range(len(chars))], key= lambda char: char[1])
+	sortedDict=collections.OrderedDict()
+	for i in range(len(chars)):
+		sortedDict[sortedArr[i][0]]={"percent": sortedArr[i][1]}
+	return sortedDict
+def CompareChars(charInfo):
+	"""
 #	englishLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 #Switch to string.ascii_uppercase
 	charIncluded = []
@@ -103,16 +116,45 @@ def CompareChars(charList, charPer):
 			print(charList[i] + " is not in the English alphabet")
 	print(charIncluded)
 	print(perIncluded)
+"""
+	charInfo=SortCharPercents(collections.OrderedDict((char, charInfo[char]) for char in charInfo if char in string.ascii_letters))
 	try:
-		alphabet = open('/alphabet.txt', "r")
-		alphabet.close()
-	except IOError as e:
+		alphabetFile = open('alphabet.txt', "r")
+	except IOError:
 		print("Could not access 'alphabet.txt' please add that to this directory.")
+	alphabetString=alphabetFile.read()
+	alphabetFile.close()
+	alphabetArr=re.findall(r"([A-Z])\s+(\d+.\d+)", alphabetString)
+	alphabetInfo=collections.OrderedDict()
+	for charArr in alphabetArr:
+		alphabetInfo[charArr[0]]={"percent": decimal.Decimal(charArr[1])}
+	alphabetInfo=SortCharPercents(alphabetInfo)
+	"""
+	if len(list(alphabetInfo.keys()))>len(list(charInfo.keys())):
+		for char in alphabetInfo:
+			if char not in charInfo:
+				alphabetInfo.pop(char)
+	else if len(list(alphabetInfo.keys()))<len(list(charInfo.keys()))
+		for char in charInfo:
+			if char not in alphabetInfo:
+				charInfo.pop(char)
+"""
+	alphabetPercents=collections.OrderedDict()
+	for char, info in alphabetInfo.items():
+		if info["percent"] not in alphabetPercents: alphabetPercents[info["percent"]]=char
+		else:alphabetPercents[info["percent"]]+=char
+	alphabetMap=collections.OrderedDict()
+	for char in charInfo:
+		lastPercent=None
+		for percent in alphabetPercents:
+			if charInfo[char]["percent"]<percent: lastPercent=percent; continue
+			alphabetMap[char]=alphabetPercents[lastPercent]
+	return alphabetMap
+	
 
 #what I have so far in a soon to be tool to compare to the english alphabet and eventually de-cipher
-"""
 def Start():
-	"Entry point" #Docstring
+	"Entry point for analyzing a single file" #Docstring
 	print("\nD:\\Code initially by Aidan Welch\n")
 	print("This is merely a tool to assist in deciphering, not an end all be all solution.  Meaning I can't guarantee this will help you.")
 	raw_input("Press Enter(Return) to continue...")
